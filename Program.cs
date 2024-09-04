a455b31552b13043bfeb11d30f7c38a3afd7111c
hussein fares <hussein.fares3@icloud.com>
2024-09-04 14:25:42 +0200

    lkjubliub

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Vad heter du?");
+string name = Console.ReadLine();
+
+// if (name.ToLower () == "vad heter du");
+
+Console.WriteLine("Hur lång är du?");
+string lång = Console.ReadLine();
+
+Console.WriteLine($"shiiit {name}! tycker fan att det där är för långt vad fan äter du! en till fråga bara!");
+Console.ReadLine();
+
+Console.WriteLine("vad Heter David?");
+string vadHeterDavid = Console.ReadLine();
+
+Console.WriteLine("lak noshit det är david men bror om du svara något annat än david bror du är duum");
+// if (lång.ToLower () == "hur lng är du");
+string laknoshitdetärdavidmenbroromdusvaranågotannatändavidbrorduärduum = Console.ReadLine();
+Console.ReadLine();
+Console.WriteLine($"Men bror {name} seriöst nu hur fan är du så jävla lång man kan inte bli {lång}cm lång utan något specielt ");
+Console.ReadLine();
+Console.WriteLine($"men ajde jalla hejdå {name}");
+Console.ReadLine();
